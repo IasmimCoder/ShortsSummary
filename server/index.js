212f6129229e534ee45dd5e9bbba95138f0e581e
Iasmim Siqueira <iasmimsas@gmail.com>
2023-09-17 15:07:13 -0300

    audio convertido

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import cors from "cors"
 import express, { response } from "express"
 
+import { convert } from "./convert.js"
 import { download } from "./download.js"
 import { transcribe } from "./transcribe.js"
 import { summarize } from "./summarize.js"
@@ -11,14 +12,15 @@ app.use(cors())
 
 app.get("/summary/:id", async (request, response) => {
   await download(request.params.id)
-  const result = await transcribe()
+  const audioConverted = await convert()
+  const result = await transcribe(audioConverted)
 
   return response.json({ result })
 })
 
 app.post("/summary", async (request, response) => {
   const result = await summarize(request.body.text)
-  response.json({ result })
+  return response.json({ result })
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
