f027e50e41fde75681cef5e35ccee918620f0b1b
Iasmim Siqueira <iasmimsas@gmail.com>
2023-09-16 21:22:00 -0300

    ajustes no resumo

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 import cors from "cors"
-import express from "express"
+import express, { response } from "express"
 
 import { download } from "./download.js"
+import { transcribe } from "./transcribe.js"
+import { summarize } from "./summarize.js"
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
-app.get("/summary/:id", (request, response) => {
-  download(request.params.id)
-  response.json({ result: "Download do vídeo realizado com sucesso!" })
+app.get("/summary/:id", async (request, response) => {
+  await download(request.params.id)
+  const result = await transcribe()
+
+  response.json({ result })
+})
+
+app.post("/summary", (request, response) => {
+  summarize(request.body.text)
+  return response.json({ result })
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
